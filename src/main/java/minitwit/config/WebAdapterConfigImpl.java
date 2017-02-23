629c34d5b96eec6c2000b2ed2125d29158423239
himanshu-purohit <mail.himanshupurohit@gmail.com>
2017-02-23 09:09:52 -0800

    commit

--- src/main/java/minitwit/config/WebAdapterConfigImpl.java
+++ src/main/java/minitwit/config/WebAdapterConfigImpl.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Created by Himanshu on 02/21/2017.
@@ -31,7 +32,7 @@ public class WebAdapterConfigImpl extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
     @Override
