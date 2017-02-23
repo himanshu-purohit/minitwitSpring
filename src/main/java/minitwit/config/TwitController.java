4ffd200725f3dbdc9fda9321bf3930e4392dc6fb
himanshu-purohit <mail.himanshupurohit@gmail.com>
2017-02-22 20:28:52 -0800

    commit

--- src/main/java/minitwit/config/TwitController.java
+++ src/main/java/minitwit/config/TwitController.java
@@ -6,6 +6,8 @@ import minitwit.model.User;
 import minitwit.service.impl.MiniTwitService;
 import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,17 +16,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.security.Principal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Controller
-public class WebConfig{
+public class TwitController {
 
     @Autowired
     private MiniTwitService service;
-    private static final String USER_SESSION_ID = "user";
 
     /*
 		 * Shows a users timeline or if no user is logged in,
@@ -33,12 +33,12 @@ public class WebConfig{
 		 *  as all the messages of followed users.
 		 */
     @GetMapping("/")
-    public String loadTimeline(Principal principal, HttpServletRequest req, Map model , HttpServletResponse res){
-
-
-        if(principal==null)
+    public String loadTimeline(HttpServletRequest req, Map model , HttpServletResponse res){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("Auth is:" + auth);
+        if(auth.getName().equals("anonymousUser"))
             return "redirect:/public";
-        User user = service.getUserbyUsername(principal.getName());
+        User user = service.getUserbyUsername(auth.getName());
         model.put("pageTitle", "Timeline");
         model.put("user",user );
         List<Message> messages = service.getUserFullTimelineMessages(user);
@@ -50,7 +50,7 @@ public class WebConfig{
              * Displays the latest messages of all users.
              */
     @GetMapping("/public")
-    public String loadPublicTimeline(HttpServletRequest req, Map model , HttpServletResponse res){
+    public String loadPublicTimeline(Map model){
 
         model.put("pageTitle", "Public Timeline");
         model.put("user", null);
@@ -62,7 +62,7 @@ public class WebConfig{
     @GetMapping("/t/{username}")
     public String loadUserTweets(HttpServletRequest req, @PathVariable String username){
         User profileUser = service.getUserbyUsername(username);
-        User authUser = service.getUserbyUsername(req.getUserPrincipal().getName());;
+        User authUser = service.getUserbyUsername(req.getUserPrincipal().getName());
         boolean followed = false;
         if(authUser != null) {
             followed = service.isUserFollower(authUser, profileUser);
@@ -79,29 +79,35 @@ public class WebConfig{
     }
 
     @GetMapping("/login")
-    public String presentLogin(HttpServletRequest req){
-        return "login.ftl";
+    public String presentLogin(HttpServletRequest request){
+        Map<String, Object> map = new HashMap<>();
+        return "login";
     }
 
     @PostMapping("/login")
     public String submitLogin(HttpServletRequest request){
+        System.out.println("Received login request");
         Map<String, Object> map = new HashMap<>();
         User user = new User();
         try {
             BeanUtils.populate(user, request.getParameterMap());
         } catch (Exception e) {
-            halt(501);
+
             return null;
         }
         LoginResult result = service.checkUser(user);
-        if(result.getUser() != null) {
-            SecurityContextHolder.getContext().setAuthentication();
-            res.redirect("/");
-            halt();
+        User authUser = result.getUser();
+        if(authUser != null) {
+            SecurityContextHolder.getContext().
+                    setAuthentication(new UsernamePasswordAuthenticationToken(authUser.getUsername(),authUser.getPassword()));
+            map.put("user",user);
+            System.out.println("User found !!!!" + user);
+            return "redirect:/";
+
         } else {
             map.put("error", result.getError());
         }
         map.put("username", user.getUsername());
-        return "login.ftl";
+        return "login";
     }
 }
--- src/main/java/minitwit/config/WebAdapterConfigImpl.java
+++ src/main/java/minitwit/config/WebAdapterConfigImpl.java
@@ -1,7 +1,56 @@
 package minitwit.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 /**
  * Created by Himanshu on 02/21/2017.
  */
-public class WebAdapterConfigImpl {
+
+@Configuration
+@EnableWebSecurity
+public class WebAdapterConfigImpl extends WebSecurityConfigurerAdapter {
+
+    private static final String[] IGNORED_STATIC_RESOURCES = {
+            "/favicon.ico",
+            "/**/*.html",
+            "/**/*.less",
+            "/**/*.css",
+            "/**/*.map",
+            "/**/*.js"
+    };
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http.authorizeRequests()
+                .antMatchers("/login", "/public","/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login");
+
+        http.csrf().disable();
+
+        // @formatter:on
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.GET, IGNORED_STATIC_RESOURCES);
+    }
+
 }
